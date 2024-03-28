9acb5e2a957dc86ec4f23fb60976fa461bd0fc00
unknown <elfilalyabdeljalil@gmail.com>
2024-03-28 14:07:46 +0000

    test 6

--- routes/api.php
+++ routes/api.php
@@ -11,16 +11,3 @@ Route::delete('/deleteUser/{user}', [AuthContoller::class, "destroy"]);
 
 Route::post("/price", [PriceEsctimateController::class, "index"]);
 Route::post('login', 'App\Http\Controllers\AuthContoller@login');
-
-
-// Route::group(
-//     [
-//         'middleware' => 'api',
-//         'prefix' => 'auth'
-//     ],
-//     function ($router) {
-//         Route::post('logout', 'AuthController@logout');
-//         Route::post('refresh', 'AuthController@refresh');
-//         Route::post('me', 'AuthController@me');
-//     }
-// );
